08d8317a35d337e7dc8ef98f76451c738f3a9f2f
amanjagdev <amankumarjagdev@gmail.com>
2020-03-30 10:21:09 +0530

    Updated Javascript functioning

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,66 @@
 const keys = {
   jsonSecret: "<YOUR_SECRET_KEY_HERE>",
   binApi: "<YOUR_BIN_API_ID_HERE>"
-}
+};
 
+//Variables
 let story;
 let storyHtml = document.querySelector("#story-here");
 let inputStory;
+let inputStoryOne;
+let inputStoryTwo;
+let inputStoryThree;
 
 //Requesting to the jsonbin
 let req = new XMLHttpRequest();
 
 req.open("GET", keys.binApi, true);
-req.setRequestHeader("secret-key",keys.jsonSecret);
+req.setRequestHeader("secret-key", keys.jsonSecret);
 req.send();
 
 req.onreadystatechange = () => {
   if (req.readyState == XMLHttpRequest.DONE) {
     story = JSON.parse(req.responseText);
-    console.log(story.content);
-    storyHtml.innerHTML= story.content
+    storyHtml.innerHTML = story.content;
   }
 };
 
+//Update Story : onbuttonclick
 const updateStory = () => {
-  inputStory = document.querySelector("#input-story").value;
-  inputStory = story.content +" "+ inputStory;
+  inputStoryOne = document.querySelector("#input-story-one").value;
+  inputStoryTwo = document.querySelector("#input-story-two").value;
+  inputStoryThree = document.querySelector("#input-story-three").value;
+  inputStory =
+    story.content +
+    " " +
+    inputStoryOne +
+    " " +
+    inputStoryTwo +
+    " " +
+    inputStoryThree;
 
   const reqToBeSend = {
-    "type" : "story",
-    "content" : inputStory
-  }
-  console.log(reqToBeSend)
-  let req2 = new XMLHttpRequest();
+    type: "story",
+    content: inputStory
+  };
 
+  //POST request to update data
+  let req2 = new XMLHttpRequest();
   req2.onreadystatechange = () => {
     if (req2.readyState == XMLHttpRequest.DONE) {
       console.log(req2.responseText);
     }
   };
-
   req2.open("PUT", keys.binApi, true);
   req2.setRequestHeader("Content-Type", "application/json");
   req2.setRequestHeader("versioning", false);
-  req2.setRequestHeader("secret-key",keys.jsonSecret);
+  req2.setRequestHeader("secret-key", keys.jsonSecret);
   req2.send(JSON.stringify(reqToBeSend));
-}
+
+  //Updating Live content
+  story.content = inputStory;
+  storyHtml.innerHTML = story.content;
+};
+
+
+
